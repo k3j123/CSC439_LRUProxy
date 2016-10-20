ffa50798d0acc6659671c60f3dfade5e1d50dcc0
ibrahimalshahrani <ibrahimalshahrani@localhost>
2016-10-20 12:31:46 -0400

    Second

---
+++ src/test/TestCacheList.java
@@ -0,0 +1,26 @@
+package test;
+
+import cs1.CacheList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class TestCacheList {
+    
+    public TestCacheList() {
+    }
+
+    @Test
+    public void testCacheList() {
+        CacheList cache = new CacheList("data", 3);
+        
+        cache.addNewObject("site1.com", false);
+        cache.addNewObject("site2.com", false);
+        cache.addNewObject("site3.com", false);
+        String url = cache.addNewObject("site4.com", false);
+        
+        assertEquals("site1.com", url);
+    }
+}
