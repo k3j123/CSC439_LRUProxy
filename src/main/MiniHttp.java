bc7ed8ba9eae3ba50347d0fbcf3757841ba00a11
lester1423 <lesterj1@nku.edu>
2016-12-05 22:05:43 -0500

    Add files via upload

---
+++ src/main/MiniHttp.java
@@ -0,0 +1,93 @@
+package main;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+
+/**
+ * MiniHttp
+ * @author Ken Cooney
+ * @date 06/11/2011
+ *
+ * This fetches the HTML page as defined URL
+ * and returns it as a StringBuffer.
+ *
+ * This can be used to save the output to a file (cache).
+ *
+ */
+public class MiniHttp
+{
+	HttpClient httpclient;
+    BufferedReader in;
+
+    public MiniHttp()
+    {
+        in = null;
+        //httpclient = HttpClientBuilder.create().build();
+        httpclient = HttpClients.custom()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                    .setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+
+    }
+
+    public StringBuffer fetch(String URL)
+    {
+
+    	StringBuffer sb = new StringBuffer("");
+
+        if (URL != null && URL.trim().length()>0)
+        {
+            // If URL doesn't start with http://, add it
+            if (! URL.startsWith("http://") &&
+                    ! URL.startsWith("https://"))
+            {
+                URL="http://"+URL;
+            }
+
+            try
+            {
+                HttpGet httpget = new HttpGet(URL);
+                HttpResponse response = httpclient.execute(httpget);
+                InputStream is = response.getEntity().getContent();
+                in = new BufferedReader(new InputStreamReader(is));
+
+                String line = "";
+                // TODO
+                while ((line = in.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                in.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Bad request " + URL);
+            	e.printStackTrace();
+            }
+            finally
+            {
+                if (in != null)
+                {
+                    try
+                    {
+                        in.close();
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return sb;
+    }
+
+}
